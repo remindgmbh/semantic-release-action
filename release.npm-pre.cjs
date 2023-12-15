b504efe1020a20016a6f87400d1e920930a52236
David Mellen <d.mellen@remind.de>
2023-12-15 10:05:28 +0100

    fix: remove tag for npm prerelease

--- release.npm-pre.cjs
+++ release.npm-pre.cjs
@@ -18,6 +18,7 @@ module.exports = {
             "@semantic-release/exec",
             {
                 prepareCmd: "npm version --no-git-tag-version ${nextRelease.version}-${nextRelease.channel ?? process.env.GITHUB_REF_NAME}-${Date.now()}-${process.env.GITHUB_SHA_SHORT}",
+                successCmd: "git push origin :refs/tags/${nextRelease.gitTag}"
             }
         ]
     ],

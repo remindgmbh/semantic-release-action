6e6d05fc2165e0aca1f3359e6486424ad95072eb
David Mellen <d.mellen@remind.de>
2024-01-29 06:54:08 +0100

    feat: disable success comments

--- release.github-npm.cjs
+++ release.github-npm.cjs
@@ -15,7 +15,12 @@ module.exports = {
             }
         ],
         "@semantic-release/npm",
-        "@semantic-release/github"
+        [
+            "@semantic-release/github",
+            {
+                successComment: false,
+            },
+        ],
     ],
     preset: "conventionalcommits"
 }
--- release.github.cjs
+++ release.github.cjs
@@ -3,12 +3,17 @@
  */
 module.exports = {
     branches: [
-        "main"
+        "main",
     ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
+        [
+            "@semantic-release/github",
+            {
+                successComment: false,
+            },
+        ],
     ],
-    preset: "conventionalcommits"
+    preset: "conventionalcommits",
 }
